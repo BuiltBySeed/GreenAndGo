b87376de9c75040d891b50b8923aa9c6ad594c01
Alex West <alex@builtbyseed.com>
2015-01-23 18:57:39 +0000

    Added standard MVC5 website

---
+++ src/GreenAndGo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GreenAndGo.Startup))]
+namespace GreenAndGo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
